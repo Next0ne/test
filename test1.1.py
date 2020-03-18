b271401f53d5f8875cacc6ea64eb95c908aa9a72
Next0ne <896202576@qq.com>
2020-03-18 09:47:55 +0800

    test

---
+++ test1.1.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : test1.1.py
+@Time : 2020/02/29 10:39:15
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+print( "我爱中国！")  # 注释1
